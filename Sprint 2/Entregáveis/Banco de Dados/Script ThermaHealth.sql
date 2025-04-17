750d57cac053fc0b369e94f4a851df2c20e6d81e
Samuel-sptech <samuel.figueiredo@sptech.school>
2025-04-17 20:36:26 -0300

    Envio da ata do dia 17/04/2025 + atualizações do banco de dados

--- Sprint 2/Entregáveis/Banco de Dados/Script ThermaHealth.sql
+++ Sprint 2/Entregáveis/Banco de Dados/Script ThermaHealth.sql
@@ -2,38 +2,38 @@ CREATE database thermaHealth;
 USE thermaHealth;
 
 CREATE TABLE hospital(
-idHospital INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(45),
-sufixo CHAR(4),
-cnpj CHAR(8),
-digitoVerifica CHAR(2),
-razaoSocial VARCHAR(200)
+	idHospital INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45)NOT NULL,
+	sufixo CHAR(4) NOT NULL,
+	cnpj CHAR(8) NOT NULL,
+	digitoVerifica CHAR(2) NOT NULL,
+	razaoSocial VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE funcionario(
-idFuncionario INT PRIMARY KEY AUTO_INCREMENT, 
-matricula VARCHAR(10),
-nome VARCHAR(100),
-senha VARCHAR(32),
-nivelAcesso CHAR(1),
-email VARCHAR(255),
-fkSupervisor INT,
-constraint fkFuncSuper foreign key (fkSupervisor)
-references funcionario(idFuncionario),
-fkHospital INT,
-constraint fkFuncHospital foreign key (fkHospital)
-references hospital(idHospital)
+	idFuncionario INT PRIMARY KEY AUTO_INCREMENT, 
+	matricula VARCHAR(10) NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	senha VARCHAR(32) NOT NULL,
+	nivelAcesso CHAR(1) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	fkSupervisor INT NOT NULL,
+		constraint fkFuncSuper foreign key (fkSupervisor)
+			references funcionario(idFuncionario),
+	fkHospital INT NOT NULL,
+		constraint fkFuncHospital foreign key (fkHospital)
+			references hospital(idHospital)
 );
 
 CREATE TABLE sala(
-idSala INT PRIMARY KEY AUTO_INCREMENT,
-setor VARCHAR(45), 
-nome VARCHAR(45),
-descricao TEXT,
-andar TINYINT, 
-fkHospital INT,
-constraint fkSalaHospital foreign key (fkHospital)
-references hospital(idHospital)
+	idSala INT PRIMARY KEY AUTO_INCREMENT,
+	setor VARCHAR(45) NOT NULL, 
+	nome VARCHAR(45) NOT NULL,
+	descricao TEXT NOT NULL,
+	andar TINYINT NOT NULL,  
+	fkHospital INT NOT NULL,
+		constraint fkSalaHospital foreign key (fkHospital)
+			references hospital(idHospital)
 );
 
 CREATE TABLE parametrosIdeais(
@@ -48,10 +48,10 @@ umidade_max INT NOT NULL
 
 CREATE TABLE sensor(
 	idSensor INT PRIMARY KEY AUTO_INCREMENT,
-	tipo VARCHAR(5),
-	numeroSerie VARCHAR(22),
-	statusSensor VARCHAR(45),
-	fkSala INT,
+	tipo VARCHAR(5) NOT NULL,
+	numeroSerie VARCHAR(22) NOT NULL,
+	statusSensor VARCHAR(45) NOT NULL,
+	fkSala INT NOT NULL,
 		constraint fkSensorSala foreign key (fkSala)
 			references sala(idSala)
 );
@@ -60,11 +60,11 @@ CREATE TABLE registro(
 	idRegistro INT,
 	fkSensor INT,
 		constraint pkRegistro primary key(idRegistro, fkSensor), 
-	temperatura FLOAT,
-	umidade INT,
-	dtHora DATETIME,
+	temperatura FLOAT NOT NULL,
+	umidade INT NOT NULL,
+	dtHora DATETIME default CURRENT_TIMESTAMP(),
 		constraint fkRegistroSensor foreign key (fkSensor)
-		references sensor(idSensor)
+			references sensor(idSensor)
 );
 
 CREATE TABLE registroAlerta(
